e9e8b5be0bc8f7c360202a6621a94c04a85d88dd
dm398 <dm398>
2017-02-18 19:34:02 +0000

    about and help pages

--- app/src/main/java/djbdevelopment/canonball/HomeActivity.java
+++ app/src/main/java/djbdevelopment/canonball/HomeActivity.java
@@ -1,5 +1,6 @@
 package djbdevelopment.canonball;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,14 +19,7 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Click button", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -41,12 +35,22 @@ public class HomeActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        Intent i = null;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_about:
+                  i = new Intent(this, SimpleContentActivity.class).putExtra(SimpleContentActivity.EXTRA_FILE,
+                        "file:///android_asset/misc/about.html");
+                startActivity(i);
+                return (true);
+
+            case R.id.action_help:
+                  i = new Intent(this, SimpleContentActivity.class).putExtra(SimpleContentActivity.EXTRA_FILE,
+                        "file:///android_asset/misc/help.html");
+                startActivity(i);
+                return (true);
         }
-
         return super.onOptionsItemSelected(item);
+
     }
 }
